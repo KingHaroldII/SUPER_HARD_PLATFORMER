2184c5dbd7aaee9d0dfacae85a59997f5e48a289
KingHaroldII <108817659+KingHaroldII@users.noreply.github.com>
2023-05-16 19:05:32 +0100

    Add files via upload

---
+++ SUPER_HARD_PLATFORMER/Game resources/sprites/Coin/coin_factory.go
@@ -0,0 +1,19 @@
+embedded_components {
+  id: "coin_factory"
+  type: "factory"
+  data: "prototype: \"/Game resources/sprites/Coin/Coin.go\"\n"
+  "load_dynamically: true\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
